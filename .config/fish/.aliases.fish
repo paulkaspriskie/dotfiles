1811edb4c3b2da3613cd3d5d71e6b02ef481ddec
Paul Kaspriskie <tug36513@stmc-l05phtdg.tu.temple.edu>
2018-09-26 10:41:03 -0400

    added fishshell .aliases file

---
+++ .config/fish/.aliases.fish
@@ -0,0 +1,9 @@
+# Navigation
+function ..    ; cd .. ; end
+function ...   ; cd ../.. ; end
+function ....  ; cd ../../.. ; end
+function ..... ; cd ../../../.. ; end
+
+# Git stuff
+alias undopush="git push -f origin HEAD^:master"
+
