267065a1a7d290b6e97d3c33de8963375f1b943b
Paul Kaspriskie <tug36513@stmc-l05phtdg.tu.temple.edu>
2019-02-13 16:01:58 -0500

    removed .config folder

--- .config/fish/.aliases.fish
+++
@@ -1,9 +0,0 @@
-# Navigation
-function ..    ; cd .. ; end
-function ...   ; cd ../.. ; end
-function ....  ; cd ../../.. ; end
-function ..... ; cd ../../../.. ; end
-
-# Git stuff
-alias undopush="git push -f origin HEAD^:master"
-
