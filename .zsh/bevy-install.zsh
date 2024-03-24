f6c8370f845314a48ae1eae062bc8d5809a4d7c7
Paul <paulkaspriskie@gmail.com>
2024-03-24 12:03:18 -0400

    chore(.zsh/bevy-install): updated formatting.

--- .zsh/bevy-install.zsh
+++ .zsh/bevy-install.zsh
@@ -11,27 +11,27 @@ ARRAY_GIT[dot-files]="git@github.com:paulkaspriskie/dotfiles.git"
 
 
 if [ -e $FILE_CONFIG ]; then
-	while read -r line; do
-		echo $line
-	done < $FILE_CONFIG
+  while read -r line; do
+    echo $line
+  done < $FILE_CONFIG
 fi
 
 
 function dir_create() {
-	if [ ! -d $1 ]; then
-		mkdir -p $1 && echo -e "${COLOR_GREEN}Created Directory: $1${COLOR_NONE}"
-	fi
+  if [ ! -d $1 ]; then
+    mkdir -p $1 && echo -e "${COLOR_GREEN}Created Directory: $1${COLOR_NONE}"
+  fi
 } && dir_create $DIR_TEST
 
 
 for key value in ${(kv)ARRAY_GIT}; do
-	IFS='-' read -r filetype address <<< "$key"
-	# echo "$filetype -> $address"
+  IFS='-' read -r filetype address <<< "$key"
+  # echo "$filetype -> $address"
 
-	if [ $PWD != $DIR_TEST ]; then
-		cd $DIR_TEST && echo "${COLOR_GREEN}Changing Directory: $DIR_TEST${COLOR_NONE}"
-	fi
+  if [ $PWD != $DIR_TEST ]; then
+    cd $DIR_TEST && echo "${COLOR_GREEN}Changing Directory: $DIR_TEST${COLOR_NONE}"
+  fi
 
-	git clone $value && echo "${COLOR_GREEN}Clone Success: $key${COLOR_NONE}"
+  git clone $value && echo "${COLOR_GREEN}Clone Success: $key${COLOR_NONE}"
 
 done && cd
